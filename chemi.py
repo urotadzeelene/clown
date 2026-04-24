ba6679bc13577108d8cfbd282bfe08c726daa3f2
urotadzeelene <urotadzeelene@gmail.com>
2026-04-24 13:07:27 +0400

    Update chemi.py

--- chemi.py
+++ chemi.py
@@ -1,16 +1,25 @@
+import random
+
+
 def draw_lines(t):
     t.speed(0)
+    t.hideturtle()
 
-    colors = ["red", "blue", "green", "yellow"]
-    lines = 12
+    colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "cyan", "magenta"]
 
-    t.pensize(6)
-
-    for i in range(lines):
+    for _ in range(60):
+        x = random.randint(-300, 300)
+        y = random.randint(-300, 300)
+        r = random.randint(80, 120)
         t.penup()
-        t.goto(0, 0)
-        t.setheading(360 / lines * i)
+        t.goto(x, y - r)
         t.pendown()
-        
-        t.color(colors[i % len(colors)])
-        t.forward(200)
+        t.color(random.choice(colors))
+        t.begin_fill()
+        t.circle(r)
+        t.end_fill()
+
+    t.penup()
+    t.goto(0, 0)
+    t.color("black")
+    t.write("thank you for playin!", align="center", font=("Arial", 24, "bold"))
